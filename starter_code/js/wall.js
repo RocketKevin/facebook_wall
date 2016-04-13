4ddc7e47a93227da0dbde86403130412531c53fe
rocketkevin <kevinchen91200@gmail.com>
2016-04-13 20:30:02 +0000

    Wall

--- starter_code/js/wall.js
+++ starter_code/js/wall.js
@@ -2,5 +2,11 @@ $(document).ready(function()
 {
 	$('#message').focus();
 
-	// YOUR CODE GOES HERE
+	$("button").click(function() {
+		var message = $("#message").val();
+		$("#wall").prepend("<div><img src='http://www.kolkatapolice.gov.in/Icons/blank-female.JPG'> </img>"+ message +"</div>");
+		$("img").width(150);
+		$("img").height(100);
+	});
 });
+
